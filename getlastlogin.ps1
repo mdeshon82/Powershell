d28a2e47bfd2cde2a1f1fa48da29395d87811827
Matt DeShon <mjdeshon82@gmail.com>
2024-03-15 11:46:22 -0400

    commit

--- getlastlogin.ps1
+++ getlastlogin.ps1
@@ -5,7 +5,18 @@ Import-Module ActiveDirectory
 $domainControllers = @("ldc-fb-dc-01.forchtbank.local", "cdc-fb-dc-01.forchtbank.local", "aznc-fb-dc-01.forchtbank.local")
 
 # Define the list of users
-$users = @("MSOL_89c7e66ac149", "SVCFBSQL", "ediscovery", "remotecredit", "svcfbcopier", "DebitCard", "DepHelp", "WiresOB", "NewAcctsExcep", "testmfa")
+$users = @(
+    "MSOL_89c7e66ac149",
+    "SVCFBSQL",
+    "ediscovery",
+    "remotecredit",
+    "svcfbcopier",
+    "DebitCard",
+    "DepHelp",
+    "WiresOB",
+    "NewAcctsExcep",
+    "testmfa"
+)
 
 # Initialize an empty array to store results
 $results = @()
@@ -15,16 +26,24 @@ foreach ($user in $users) {
     # Initialize an empty hashtable to store user information
     $userInfo = @{}
 
+    # Add username to user information
+    $userInfo["Username"] = $user
+
     # Loop through each domain controller
     foreach ($dc in $domainControllers) {
-        # Get last login time from each domain controller
-        $lastLogon = Get-ADUser $user -Server $dc -Properties LastLogon | Select-Object -ExpandProperty LastLogon
+        # Get last logon timestamp from each domain controller
+        $userObject = Get-ADUser $user -Server $dc -Properties LastLogonTimestamp
 
-        # Convert last logon time to readable format
-        $lastLogonFormatted = [DateTime]::FromFileTime($lastLogon)
+        # Check if LastLogonTimestamp is not null
+        if ($userObject.LastLogonTimestamp -ne $null) {
+            # Convert last logon timestamp to readable format
+            $lastLogonFormatted = [DateTime]::FromFileTime([Int64]::Parse($userObject.LastLogonTimestamp))
 
-        # Add last logon time for current domain controller to user information
-        $userInfo[$dc] = $lastLogonFormatted
+            # Add last logon time for current domain controller to user information
+            $userInfo[$dc] = $lastLogonFormatted
+        } else {
+            $userInfo[$dc] = "Last logon not available"
+        }
     }
 
     # Add user information to results array
